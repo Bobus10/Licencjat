5860a26fa79d3710b36a1678f0e763a0fb7682cf
Bobus10 <78688548+Bobus10@users.noreply.github.com>
2023-04-25 01:18:00 +0200

    Role uzytkownika i administratora

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,8 +61,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/users/list">{{ __('shop.user.index_title') }}</a>
-                                    <a class="dropdown-item" href="{{ route('products.index') }}">{{ __('shop.product.index_title') }}</a>
+                                    @can('isAdmin')
+                                        <a class="dropdown-item" href="/users/list">{{ __('shop.user.index_title') }}</a>
+                                        <a class="dropdown-item" href="{{ route('products.index') }}">{{ __('shop.product.index_title') }}</a>
+                                    @endcan
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
